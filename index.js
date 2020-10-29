21b65cb508f0ab099fc5286664b54359265efe2e
Henry <63872314+Fingolfin7@users.noreply.github.com>
2020-10-30 00:54:20 +0200

    Implemented error checking

--- index.js
+++ index.js
@@ -5,22 +5,51 @@ function sendEmail(){
   var phoneNumber = document.getElementById('phoneNumber').value;
   var email = document.getElementById('email').value;
   var myMessage = document.getElementById('message').value;
+  var error_message= document.getElementById('error_message');
+  var correctFormat = true;
+  var text;
+
+  error_message.style.padding= "10px";
 
   console.log(firstName + " " + surname);
   console.log(email);
   console.log(myMessage);
 
+//check if form sections have been filled in properly:
+
+if(firstName.length == 0 || surname.length == 0){
+  text = " *Enter valid Name and Surname* ";
+  error_message.innerHTML = text;
+  correctFormat = false;
+}
+
+else if(isNaN(phoneNumber) || phoneNumber.length !=10){
+  text = " *Enter valid phone number* <br> <em>*No spaces*</em> ";
+  error_message.innerHTML=text;
+  correctFormat = false;
+}
+
+else if(email.indexOf("@") == -1){//checking for the @ symbol on the email address
+  text = " *Enter valid email address* ";
+  error_message.innerHTML=text;
+  correctFormat = false;
+}
+
   //send email
-  Email.send({
-	Host: "smtp.gmail.com",
-  Security: 'ssl',
-	Username : "contactKudaMushunje@gmail.com",
-	Password : "wordpassmus092",
-	To : "mushunjek@gmail.com",
-	From : "contactKudaMushunje@gmail.com",
-	Subject : "CV Website Contact from: " + String(firstName) + " " + String(surname),
-	Body : String(myMessage) + "<br><br>Phone Number: " + String(phoneNumber) + "<br><br>Email Address: " +  String(email),
-	}).then(function(message){
-    alert("mail sent successfully")
-  });
+  if(correctFormat){
+    error_message.innerHTML = "";
+    error_message.style.padding = "0px";
+    Email.send({
+  	Host: "smtp.gmail.com",
+    	Security: 'ssl',
+  	Username : "contactKudaMushunje@gmail.com",
+  	Password : "wordpassmus092",
+  	To : "mushunjek@gmail.com",
+  	From : "contactKudaMushunje@gmail.com",
+  	Subject : "CV Website Contact from: " + String(firstName) + " " + String(surname),
+  	Body : String(myMessage) + "<br><br>Phone Number: " + String(phoneNumber) + "<br><br>Email Address: " +  String(email),
+  	}).then(function(message){
+      alert("mail sent successfully")
+    });
+  }
 }
